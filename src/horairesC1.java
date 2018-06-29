025d6d653a9909170f9df5c3b2a4def459c521e4
LOTONGA Eliam <eliam.lotonga@ives.fr>
2018-06-29 15:08:30 +0200

    Optimisation du code

--- src/horairesC1.java
+++ src/horairesC1.java
@@ -1,14 +1,8 @@
-import java.awt.Component;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Iterator;
 import java.util.Timer;
@@ -16,7 +10,6 @@ import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -37,214 +30,132 @@ public class horairesC1 implements Runnable {
 		this.frame=fenetre;
 		this.lblMaupertuis=lblMaupertuis;
 		this.lblJeanMace=lblJeanMace;
+	}
+	
+	
+	private void MajC1(String p_url) 
+	{
 
-		Timer timer = new Timer();
-		timer.schedule(new TimerTask() {
-			public void run()
-			{
-				MajC1Maupertuis();
-				MajC1JeanMace();
-				frame.repaint();
-			}
+		Calendar cal =  Calendar.getInstance();
+		this.minutes = cal.get(Calendar.MINUTE);
+		this.heures =cal.get(Calendar.HOUR_OF_DAY) ;
+		this.day = cal.get(Calendar.DAY_OF_WEEK);
 
-			private void MajC1JeanMace() {
-				Calendar cal =  Calendar.getInstance();
-				minutes = cal.get(Calendar.MINUTE);
-				heures =cal.get(Calendar.HOUR_OF_DAY) ;
-				int day = cal.get(Calendar.DAY_OF_WEEK);
+		try {
+			String ecrire ="";
+			boolean J = true;
 
-				ArrayList<String> listeHorairesJeanMace = new ArrayList();
-				//System.out.println("Jean Mace <-- C1");
+			JSONParser parser = new JSONParser();
+			//https://data.metromobilite.fr/api/routers/default/index/stops/SEM:1994"+"/stoptimes
+			URL web = new URL(p_url);
 
-				try {
-					String ecrire ="";
-					boolean J = true;
+			URLConnection connect = web.openConnection();
+			//Buffered in buffer
+			BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
 
-					int heuresMaupertuis = heures;
-					int minutesMaupertuis = minutes;
+			JSONArray jsonObject = (JSONArray) parser.parse(in);
 
-					int heuresJeanMace = heuresMaupertuis;
-					int minutesJeanMace=minutesMaupertuis;
+			Iterator i = jsonObject.iterator();
 
-					/*********************************************************************************************************************/
+			while(i.hasNext()) {
+				JSONObject innerObj = (JSONObject) i.next();
+				//System.out.println("times is : " + innerObj.get("times"));
 
-					JSONParser parser = new JSONParser();
+				JSONArray array = (JSONArray) innerObj.get("times");
 
-					URL web = new URL("https://data.metromobilite.fr/api/routers/default/index/stops/SEM:1994"+"/stoptimes");
-
-					URLConnection connect = web.openConnection();
-					//Buffered in buffer
-					BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-
-					JSONArray jsonObject = (JSONArray) parser.parse(in);
-
-					Iterator i = jsonObject.iterator();
-
-					while(i.hasNext()) {
-						JSONObject innerObj = (JSONObject) i.next();
-						//System.out.println("times is : " + innerObj.get("times"));
-
-						JSONArray array = (JSONArray) innerObj.get("times");
-
-						Iterator j = array.iterator();
-						int verite = 0;
-						while(j.hasNext()==J) {
-							if(verite==1) {
-								J=false;
-							}else {
-								JSONObject Obj = (JSONObject) j.next();
-								ecrire = (String) Obj.get("realtimeArrival").toString();
-								//System.out.println("Arrivé prévu " + Obj.get("realtimeArrival"));
-								verite++;
-							}
-						}
-						int intParse = Integer.parseInt(ecrire);
-
-						//System.out.println(intParse);
-						float c = intParse;
-						int h = intParse;
-						int b = 3600;
-
-						float divided = h/b;
-						float reel = c/b;
-
-						//System.out.println(reel);
-						//System.out.println(divided);
-
-						float resultatFinal = (float) ((((reel-divided)/100)*60*100)+0.0001);
-
-						System.out.println("Le prochain passage du bus est a "+(int)divided+":"+(int)resultatFinal );
-
-						if(minutesJeanMace<10){
-							lblJeanMace.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
-						}else{
-							lblJeanMace.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
-						}
-
+				Iterator j = array.iterator();
+				int verite = 0;
+				while(j.hasNext()==J) {
+					if(verite==1) {
+						J=false;
+					}else {
+						JSONObject Obj = (JSONObject) j.next();
+						ecrire = (String) Obj.get("realtimeArrival").toString();
+						//System.out.println("Arrivé prévu " + Obj.get("realtimeArrival"));
+						verite++;
 					}
+				}
+				int intParse = Integer.parseInt(ecrire);
 
-					in.close();
+				//System.out.println(intParse);
+				float c = intParse;
+				int h = intParse;
+				int b = 3600;
 
+				float divided = h/b;
+				float reel = c/b;
 
+				//System.out.println(reel);
+				//System.out.println(divided);
 
+				float resultatFinal = (float) ((((reel-divided)/100)*60*100)+0.0001);
 
+				System.out.println("Le prochain passage du bus est a "+(int)divided+":"+(int)resultatFinal );
 
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (ParseException e) {
-					System.out.println("Pb de parse");
-					e.printStackTrace();
+				if(this.minutes<10){
+					lblMaupertuis.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
+				}else{
+					lblMaupertuis.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
 				}
 
-
 			}
-
-			private void MajC1Maupertuis() {
-
-				Calendar cal =  Calendar.getInstance();
-				minutes = cal.get(Calendar.MINUTE);
-				heures =cal.get(Calendar.HOUR_OF_DAY) ;
-				int day = cal.get(Calendar.DAY_OF_WEEK);
-
-				ArrayList<String> listeHorairesMaupertuis = new ArrayList();
-				System.out.println("C1--> Maupertuis");
-
-				try {
-					String ecrire ="";
-					boolean J = true;
-
-					int heuresMaupertuis = heures;
-					int minutesMaupertuis = minutes;
-
-					int heuresJeanMace = heuresMaupertuis;
-					int minutesJeanMace=minutesMaupertuis;
-
-					/*********************************************************************************************************************/
-
-					JSONParser parser = new JSONParser();
-
-					URL web = new URL("https://data.metromobilite.fr/api/routers/default/index/stops/SEM:1995"+"/stoptimes");
-
-					URLConnection connect = web.openConnection();
-					//Buffered in buffer
-					BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-
-					JSONArray jsonObject = (JSONArray) parser.parse(in);
-
-					Iterator i = jsonObject.iterator();
-
-					while(i.hasNext()) {
-						JSONObject innerObj = (JSONObject) i.next();
-						//System.out.println("times is : " + innerObj.get("times"));
-
-						JSONArray array = (JSONArray) innerObj.get("times");
-
-						Iterator j = array.iterator();
-						int verite = 0;
-						while(j.hasNext()==J) {
-							if(verite==1) {
-								J=false;
-							}else {
-								JSONObject Obj = (JSONObject) j.next();
-								ecrire = (String) Obj.get("realtimeArrival").toString();
-								//System.out.println("Arrivé prévu " + Obj.get("realtimeArrival"));
-								verite++;
-							}
-						}
-						int intParse = Integer.parseInt(ecrire);
-
-						//System.out.println(intParse);
-						float c = intParse;
-						int h = intParse;
-						int b = 3600;
-
-						float divided = h/b;
-						float reel = c/b;
-
-						//System.out.println(reel);
-						//System.out.println(divided);
-
-						float resultatFinal = (float) ((((reel-divided)/100)*60*100)+0.0001);
-
-						System.out.println("Le prochain passage du bus est a "+(int)divided+":"+(int)resultatFinal );
-
-						if(minutesJeanMace<10){
-							lblMaupertuis.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
-						}else{
-							lblMaupertuis.setText("Passage a "+(int)divided+":"+(int)resultatFinal);
-						}
-
-					}
+		}catch (IOException | ParseException e1) {
+			e1.printStackTrace();
+		}
+	}
 
 
+	public int getHeures() {
+		return heures;
+	}
 
 
+	public void setHeures(int heures) {
+		this.heures = heures;
+	}
 
 
-				}catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally{
-				};
-			}
+	public int getMinutes() {
+		return minutes;
+	}
 
-		},0, 10000);
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+
+
+	public int getDay() {
+		return day;
+	}
+
+
+	public void setDay(int day) {
+		this.day = day;
 	}
 
 
 	@Override
 	public void run() {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			public void run()
+			{
+				//Maupertuis
+				MajC1("https://data.metromobilite.fr/api/routers/default/index/stops/SEM:1995/stoptimes");
+				//JeanMace
+				MajC1("https://data.metromobilite.fr/api/routers/default/index/stops/SEM:1994/stoptimes");
+				
+				frame.repaint();
+			}
+
+		},0, 10000);
+		
 	}
+	
+	
+	
+
 }
 
 
